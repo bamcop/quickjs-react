0a86af4398dccccfb077c484ab8efa1b89e52489
lifeng <lifeng9204@qq.com>
2023-09-21 00:28:23 +0800

    feat: add example for use golang + quickjs to run render_to_string/index.bundle_esbuild_cli.js     Todo: fix ReferenceError: 'process' is not defined

---
+++ examples/qucikjs_render/main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"os"
+	"path/filepath"
+	"time"
+
+	"github.com/bamcop/kit"
+	"github.com/bamcop/kit/debug"
+	"github.com/buke/quickjs-go"
+	polyfill "github.com/buke/quickjs-go-polyfill"
+)
+
+var (
+	workingDir string
+	srcCode    string
+)
+
+func init() {
+	workingDir = debug.MustMainFileDir()
+
+	filename := filepath.Join(workingDir, "../render_to_string/index.bundle_esbuild_cli.js")
+	b, err := os.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	srcCode = string(b)
+}
+
+func main() {
+	// Create a new runtime
+	rt := quickjs.NewRuntime()
+	defer rt.Close()
+
+	// Create a new context
+	ctx := rt.NewContext()
+	defer ctx.Close()
+
+	// Inject polyfills to the context
+	polyfill.InjectAll(ctx)
+
+	ret, err := ctx.Eval(srcCode)
+	defer ret.Free()
+	kit.Try(err)
+
+	// Wait for the timeout to finish
+	time.Sleep(time.Millisecond * 100)
+
+	err = rt.ExecuteAllPendingJobs()
+	kit.Try(err)
+}
