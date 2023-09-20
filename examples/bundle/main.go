d88e367e1b0c0173369901ba690516d791a6f69b
lifeng <lifeng9204@qq.com>
2023-09-20 23:40:46 +0800

    chore: cmd/bundle -> examples/bundle

--- examples/bundle/main.go
+++ examples/bundle/main.go
@@ -2,19 +2,14 @@ package main
 
 import (
 	"os"
-	"path/filepath"
 
-	"github.com/bamcop/kit"
 	"github.com/bamcop/kit/debug"
 	"github.com/evanw/esbuild/pkg/api"
 )
 
 func main() {
-	workingDir, err := filepath.Abs(filepath.Join(debug.MustMainFileDir(), "../.."))
-	kit.Try(err)
-
 	result := api.Build(api.BuildOptions{
-		AbsWorkingDir: workingDir,
+		AbsWorkingDir: debug.MustMainFileDir(),
 		EntryPoints:   []string{"input.js"},
 		Outfile:       "output.js",
 		Bundle:        true,
