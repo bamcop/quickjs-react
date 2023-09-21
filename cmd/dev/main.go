b8e463f07e9e9416bba5d89bf8125bf9d8c59972
lifeng <lifeng9204@qq.com>
2023-09-21 16:13:53 +0800

    feat: add public/favicon.ico feat: dev serve add handler for index.html

--- cmd/dev/main.go
+++ cmd/dev/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"net/http"
+	"os"
 	"path/filepath"
 
 	"github.com/bamcop/kit"
@@ -22,6 +24,19 @@ func main() {
 	r := gin.Default()
 
 	r.Static("/static", filepath.Join(RootDir, "public"))
+	r.StaticFile("/favicon.ico", filepath.Join(RootDir, "public", "favicon.ico"))
+
+	r.GET("/", func(c *gin.Context) {
+		b, err := os.ReadFile(filepath.Join(RootDir, "public/examples/hello.html"))
+		if err != nil {
+			c.AbortWithError(http.StatusInternalServerError, err)
+			return
+		}
+
+		c.Writer.Header().Set("Content-Type", `text/html; charset=UTF-8`)
+		_, _ = c.Writer.WriteString(string(b))
+		c.Status(http.StatusOK)
+	})
 
 	r.Run(":4399")
 }
